15f56a6c384ecea319009ac96b9d72107d590fd9
BBBOND <jwy8645@163.com>
2018-05-08 23:59:35 +0800

    样式调整

--- app/src/main/java/com/bbbond/androidStudyKT/home/MainPresenter.kt
+++ app/src/main/java/com/bbbond/androidStudyKT/home/MainPresenter.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.ViewModelProviders
 import androidx.core.net.toUri
 import com.alibaba.android.arouter.launcher.ARouter
 import com.bbbond.androidStudyKT.data.ActItem
-import org.jetbrains.anko.toast
 
 class MainPresenter(
         var activity: MainActivity
@@ -26,7 +25,7 @@ class MainPresenter(
     }
 
     override fun jump(actItem: ActItem) {
-        activity.toast(actItem.path)
+//        activity.toast(actItem.path)
         ARouter
                 .getInstance()
                 .build(actItem.path.toUri())
